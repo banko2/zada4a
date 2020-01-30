98fbfca14f3b17113b0928f74dbc6bb3beedf1e3
banko2 <60343173+banko2@users.noreply.github.com>
2020-01-30 11:40:13 +0200

    add style.css

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dama</title>
+    <title>Izbor</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Woman galeri</h1>
-    <img src="ital1.jpg" alt="Italian girl">
+    <h1>Woman galery</h1>
+    <img src="/zada4a/images/ital1.jpg" alt="Italian girl">
 </body>
 </html>
