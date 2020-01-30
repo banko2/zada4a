f849c4835819ae67af2379bca2c89f044e102e23
banko2 <60343173+banko2@users.noreply.github.com>
2020-01-30 12:50:27 +0200

    obed2

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+
+<?php
+    session_start();
+
+    if (!isset($_SESSION['womanList'])) {
+        $_SESSION['womanList'] = array();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,12 +16,23 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <div class ="content">
     <h1>Woman galery</h1>
+    <img src="/zada4a/images/Wgalery.jpg" alt="Italian girl">
+
     <a class="button" href="/zada4a/images/ital1.jpg">Choise ital.</a>
     <a class="button" href="/zada4a/images/bulg.jpg">Choise bulg.</a>
     <a class="button" href="/zada4a/images/usa.jpg">Choise usa.</a>
 
-    <img src="/zada4a/images/ital1.jpg" alt="Italian girl">
+    <ul class="woman-list">
+            <?php
+                foreach ($_SESSION['womanList'] as $womanName) {
+                    echo "<li>$womanName</li>";
+                }
+            ?>
+        </ul>
+            </div>
+
    
 </body>
 </html>
